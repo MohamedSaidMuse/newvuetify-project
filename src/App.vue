fb068b8b967450c89dc82034b9a300b210118fbb
MohamedSaidMuse <51102939+MohamedSaidMuse@users.noreply.github.com>
2024-06-16 12:55:58 -0700

    Icons

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,32 @@
     >
   <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
   <v-divider></v-divider>
-  <v-list-item link title="List Item 1"></v-list-item>
-  <v-list-item link title="List Item 2"></v-list-item>
-  <v-list-item link title="List Item 3"></v-list-item>
+<v-list-item link>
+  <v-list-item-icon>
+    <v-icon>mdi-home</v-icon>
+  </v-list-item-icon>
+  <v-list-item-content>
+    <v-list-item-title>Home</v-list-item-title>
+  </v-list-item-content>
+</v-list-item>
+<v-list-item link>
+  <v-list-item-icon>
+    <v-icon>mdi-format-list-bulleted-type</v-icon>
+  </v-list-item-icon>
+  <v-list-item-content>
+    <v-list-item-title>Types</v-list-item-title>
+  </v-list-item-content>
+</v-list-item>
+<v-list-item link>
+  <v-list-item-icon>
+    <v-icon>mdi-view-list</v-icon>
+  </v-list-item-icon>
+  <v-list-item-content>
+    <v-list-item-title>Categories</v-list-item-title>
+  </v-list-item-content>
+</v-list-item>
 </v-navigation-drawer>
+  
 
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -24,18 +46,24 @@
   </v-app>
 </template>
 
+
 <script setup>
   import { ref } from 'vue'
+  import '@mdi/font/css/materialdesignicons.css'; // Ensure you import the MDI CSS
 
   const drawer = ref(null)
 </script>
 
 <script>
+/*<!--<v-list-item link title="Home"></v-list-item>
+  <v-list-item link title="Types"></v-list-item>
+  <v-list-item link title="Categrories"></v-list-item>-->*/
   export default {
     data: () => ({ drawer: null,
   items: [
-    {title:'Dashboard'  , icon:'mdi-view-dashboard'},
-    {title:'About'  , icon:'mdi-help-box'},
+    {title:'Home'  , icon:'mdi-view-dashboard'},
+    {title:'Types'  , icon:'mdi-help-box'},
+    {title:'Categrories'  , icon:'mdi-help-box'},
     ],
      }),
   }
